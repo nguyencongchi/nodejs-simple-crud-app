fd80737c3a1fa5c926ce05dbbba985c6f4e3cead
Nguyen Cong Chi <nguyencongchi@192.168.1.11>
2024-11-28 06:56:31 +0700

    message clear when create product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,7 +3,7 @@ const Product = require("../models/product.model.js");
 const createProduct = async (req, res) => {
     try {
         const product = await Product.create(req.body);
-        res.status(200).json({ message: "Product created" });
+        res.status(200).json({ "Success": true, message: "Product created", "Product": product });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
